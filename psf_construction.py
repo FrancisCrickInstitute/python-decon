0cb9ef307ad9748a9ef7eacda9c657d1327e4384
Dave Barry <david.barry@crick.ac.uk>
2024-08-06 15:45:53 +0100

    Update psf_construction.py

--- psf_construction.py
+++ psf_construction.py
@@ -1,24 +1,7 @@
-import numpy as np
 import psfmodels as psfm
-from pycudadecon import decon
-from skimage import io
-from PIL import Image
-
-nx = 11
-nz = 11
+from tifffile import imwrite
 
 # generate centered psf with a point source at `pz` microns from coverslip
 # shape will be (127, 127, 127)
-#psf = psfm.make_psf(np.linspace(-5, 5, 51), 127, dxy=0.05, dz=0.05, pz=0, ni=1.4, ns=1.443, NA=1.25, wvl=0.42)
-
-#io.imsave('./psf.tiff', psf)
-
-im = Image.open('./DAPI.tif')
-
-image = np.array(im)
-
-psf = io.imread('./psf.tiff')
-
-result = decon(image, psf)
-
-io.imsave('./DAPI_decon.tiff', result)
+psf = psfm.make_psf(61, 32, dxy=0.227, dz=0.5, pz=0, ti0=610, wvl=0.465, NA=0.8)
+imwrite('./psf.tiff', psf)
