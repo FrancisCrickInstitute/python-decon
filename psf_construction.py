d3ad65ed581f3be8b080b2935aff03a5d62bb35a
David Barry <david.barry@crick.ac.uk>
2024-08-07 14:42:46 +0100

    Updated requirements.txt

--- psf_construction.py
+++
@@ -1,7 +0,0 @@
-import psfmodels as psfm
-from tifffile import imwrite
-
-# generate centered psf with a point source at `pz` microns from coverslip
-# shape will be (127, 127, 127)
-psf = psfm.make_psf(61, 32, dxy=0.227, dz=0.5, pz=0, ti0=610, wvl=0.465, NA=0.8)
-imwrite('./psf.tiff', psf)
